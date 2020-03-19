e1dddf79a687390f7bffa331bb653430e77fb53d
antibrand <antibrand@ccdzine.com>
2020-03-18 23:24:49 -0800

    Removed unused files. Demo page CSS.

--- react/svg.js
+++
@@ -1,5 +0,0 @@
-export default ( props ) => (
-	<svg { ...props } >
-		{ props.children }
-	</svg>
-);
--- react/svg.native.js
+++
@@ -1,7 +0,0 @@
-import Svg from 'react-native-svg';
-
-export default ( props ) => (
-	<Svg width={ props.width } height={ props.height } >
-		{ props.children }
-	</Svg>
-);
